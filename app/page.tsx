f1e84066426eefd3c351fc8c45de8488b3fc1b95
KunalMenon <15151339+K-M-Code@users.noreply.github.com>
2024-10-05 17:57:27 +0300

    Info Content Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 import HeroSection from "@/components/frontend/HeroSection";
 import Header from "@/components/frontend/Header";
-import InfoSection1 from "@/components/frontend/InfoSection1";
+import InfoSection from "@/components/frontend/InfoSection";
 
 
 
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Header/>
       <HeroSection />
-      <InfoSection1 />
+      <InfoSection />
     </>
   );
 }
