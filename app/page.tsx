4f22e8f86d09cc5141fc7a0a8118ef621210d431
KunalMenon <15151339+K-M-Code@users.noreply.github.com>
2024-10-05 11:28:04 +0300

    Basic Structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-        priority
-      />
+      <h1>Home Page</h1>
     </div>
   );
 }
